e80190e95f5e4ccd6e40ab16fbaa46fdbc086068
Naqeeb Rehman <naq.rehman@gmail.com>
2023-02-10 19:34:45 -0500

    Trainer class file

---
+++ src/trainer.cpp
@@ -0,0 +1,16 @@
+#include "config.h"
+#include "network.cpp"
+
+class Trainer {
+public:
+
+    /**
+     * @brief Create a new trainer object
+     *
+     * @param network Network trainer is being created for
+     */
+    Trainer(Network network) {
+
+    }
+
+};
