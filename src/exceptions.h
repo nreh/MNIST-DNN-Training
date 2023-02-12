c909100a7e74067ce532c9d74dd421ab3541fc0e
Naqeeb Rehman <naq.rehman@gmail.com>
2023-02-12 16:44:12 -0500

    Exceptions & invalid_function_call exception

---
+++ src/exceptions.h
@@ -0,0 +1,23 @@
+#include <stdexcept>
+#include <string>
+
+/**
+ * https://stackoverflow.com/a/17438895
+ */
+
+ /**
+  * @brief Thrown when an invalid function is invoked.
+  */
+class invalid_function_call: public std::exception {
+private:
+    std::string message;
+public:
+    explicit invalid_function_call(const std::string& message);
+    const char* what() const noexcept override {
+        return this->message.c_str();
+    }
+};
+
+invalid_function_call::invalid_function_call(const std::string& message): message(message) {
+
+}
